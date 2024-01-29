5df75a7947d1c801332faf4279755273a77c8070
Nikhil <nikjjin9165@gmail.com>
2024-01-29 18:49:20 +0530

    Changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -81,7 +81,7 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
@@ -91,7 +91,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
